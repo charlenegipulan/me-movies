1d15ef18a25f101b0dcd715730f18bfbd1784b96
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-08-19 16:56:23 -0700

    delete movie functionality

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,6 +6,12 @@ class Movies extends Component {
         movies: getMovies()
     };
 
+    handleDelete = (movie) => {
+        //get all movies except the one that was passed
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({ movies });
+    };
+
     render() {
         return (
             <table className="table">
@@ -15,15 +21,19 @@ class Movies extends Component {
                         <th>Genre</th>
                         <th>Stock</th>
                         <th>Rate</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     { this.state.movies.map(movie => (
-                        <tr>
+                        //uniquely identifies each movie
+                        <tr key={movie._id}>
                             <td>{movie.title}</td>
                             <td>{movie.genre.name}</td>
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
+                            <td>
+                                <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-small">Delete</button></td>
                         </tr>
                     ))}
                 </tbody>
